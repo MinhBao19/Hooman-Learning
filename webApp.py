da861e968876663f1ba072a71b6e8a5881f99f2a
Luis Sanabria <91412655+Luie2212@users.noreply.github.com>
2025-08-30 18:52:47 +1000

    Added textbox backend-front end tunnel

--- webApp.py
+++ webApp.py
@@ -1,13 +1,25 @@
 #!/bin/python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+saved_text = ""
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", saved_text=saved_text)
 
+@app.route("/submit", methods=["POST"])
+def submit_text():
+    
+    global saved_text
+    # get the value from the form (the "name" of the input box)
+    saved_text = request.form.get("user_text")
+    
+    output_text = f"User entered: {saved_text}"  # display to output_text 
+
+    return render_template("index.html", saved_text=saved_text, output_text=output_text)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=True)
