23289293a444b297f54c089f51241e934ed6fc6c
Luis Sanabria <91412655+Luie2212@users.noreply.github.com>
2025-08-30 22:20:25 +1000

    Added all Ranking info output to webpage

--- webApp.py
+++ webApp.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template, request
 from OpenAIInterface import callOpenAI
+import re
+
 
 app = Flask(__name__)
 
@@ -21,10 +23,15 @@ def submit_text():
     user_args = request.form.get("user_args")
 
     saved_text = f"supplier: Woolworths, item_name:{user_item}, sort_by:{user_category}"
-    
     output_text = callOpenAI(saved_text)
 
+    # Access variables
+    rank1_name = output_text.rank1_name
+    rank1_scoreReasoning = output_text.rank1_scoreReasoning
 
+    print(rank1_name)
+    print(rank1_scoreReasoning[:60], "...")
+    
     return render_template("index.html", saved_text=saved_text, output_text=output_text)
 
 
