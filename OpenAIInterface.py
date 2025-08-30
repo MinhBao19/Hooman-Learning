105ee60df81e2f43a225823eb0680c989e5af8ba
Luis Sanabria <91412655+Luie2212@users.noreply.github.com>
2025-08-30 21:31:56 +1000

    Tweaked AI outputs

--- OpenAIInterface.py
+++ OpenAIInterface.py
@@ -9,23 +9,26 @@ def callOpenAI(prompt: str) -> str:
         rank1_price: str
         rank1_sustainability_score10: str
         rank1_health_score10: str
+        rank1_scoreReasoning: str
 
         rank2_name: str
         rank2_price: str
         rank2_sustainability_score10: str
         rank2_health_score10: str
+        rank2_scoreReasoning: str
 
         rank3_name: str
         rank3_price: str
         rank3_sustainability_score10: str
         rank3_health_score10: str
+        rank3_scoreReasoning: str
 
     response = client.responses.parse(
         model="gpt-4o-2024-08-06",
         input=[
             {
                 "role": "system",
-                "content": "You are an datasorting algorithm returning the highest scoring product from a user specified source. The source will be in the form of a supplier name. You will recieve a range of user inputs which you will use to find and rank products actively sold by this supplier and format it into an output matching the datastructure given to you",
+                "content": "You are an datasorting algorithm returning the highest scoring product from a user specified source. The source will be in the form of a supplier name. You will recieve a range of user inputs which you will use to find and rank products actively sold by this supplier and format it into an output matching the datastructure given to you. Summaries produced must relate to scores given.",
             },
             {"role": "user", "content": f"{prompt}"},
         ],
